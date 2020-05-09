be92a1bbd61802d564bf258eb8328543259da152
unknown <fawwadahmed3@gmail.com>
2020-05-10 01:12:44 +0500

    Login With Google

--- LoginReactNative/App.js
+++ LoginReactNative/App.js
@@ -7,6 +7,7 @@ import {
   GraphRequest,
   GraphRequestManager,
 } from 'react-native-fbsdk';
+import { GoogleSignin, GoogleSigninButton, statusCodes } from '@react-native-community/google-signin';
 
 export default class App extends Component {
   constructor() {
@@ -24,12 +25,33 @@ export default class App extends Component {
       //Alert for the Error
       Alert.alert('Error fetching data: ' + error.toString());
     } else {
-      console.warn(result);
       //response alert
       alert(JSON.stringify(result));
       this.setState({ user_name: 'Welcome' + ' ' + result.name });
       this.setState({ token: 'User Token: ' + ' ' + result.id });
       this.setState({ profile_pic: result.picture.data.url });
+    }
+  };
+  _signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      console.warn(userInfo);
+      alert(userInfo);
+      this.setState({ user_name: 'Welcome' + ' ' + userInfo.user.name });
+      this.setState({ token: 'User Token: ' + ' ' + userInfo.user.id });
+      this.setState({ profile_pic: userInfo.user.photo });
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+      console.log(error)
     }
   };
 
@@ -74,8 +96,27 @@ export default class App extends Component {
           }}
           onLogoutFinished={this.onLogout}
         />
+        <GoogleSigninButton
+          style={{ width: 192, height: 48 }}
+          size={GoogleSigninButton.Size.Wide}
+          color={GoogleSigninButton.Color.Dark}
+          onPress={this._signIn}
+          disabled={this.state.isSigninInProgress} 
+          onLogoutFinished={this.onLogout}/>
       </View>
     );
+  }
+  componentWillMount(){
+    GoogleSignin.configure({
+      scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
+      webClientId: '174851680298-jkbnjclicjcj15kjqsb9rk5crg8dercc.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+      offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+      // hostedDomain: '', // specifies a hosted domain restriction
+      // loginHint: '', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
+      forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
+      // accountName: '', // [Android] specifies an account name on the device that should be used
+      //iosClientId: '<FROM DEVELOPER CONSOLE>', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+    });
   }
 }
 
